a89c03ea45cf37989bb4e600d34f36c28baabee5
FENILJAGANI <103998213+FENILJAGANI0309@users.noreply.github.com>
2022-04-19 19:36:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,2 @@
-streamlit.title(' My Moms New Healthy Diner ')
-##streamlit.header('Breakfast Favorites')
-##streamlit.text(' Omega 3 & Blueberry Oatmeal')
-#streamlit.text(' Kale, Spinach & Rocket Smoothie')
-#streamlit.text(' Hard-Boiled Free-Range Egg')
-#streamlit.text(' Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
