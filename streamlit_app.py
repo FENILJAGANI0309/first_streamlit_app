5ee26f3441703796d55ffce1381a82a3c7cd097d
FENILJAGANI <103998213+FENILJAGANI0309@users.noreply.github.com>
2022-04-29 15:18:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # Take json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
